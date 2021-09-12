31a191862acbda8395c50ea9b9885ef02119f447
lisahuele <lisahuele@gmail.com>
2021-09-12 02:23:10 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Home from './components/Home'
 
 function App() {
+  const [homeState] = useState(true);
+
+
+
   return (
     <body>
       <Home/>
-      <Header/>
+      {!homeState && <Header/>}
 
       <main>
         -- ABOUT - when selected display more info
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Header from '../Header';
 
 function Home() {
     return (
-        <div className="hero-section">
-          <p>Lisa Le - Full Stack Developer</p>
-          <h1>The devil is in the details,<br/>but the
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            is in the experience.</h1>
-            <img className="image" src={require('../../assets/images/Soul.png').default} alt='soul'/>
-      </div>
+        <div className="hero-background">
+            <div className="hero-text">
+                <p>Lisa Le - Full Stack Developer</p>
+                <h1>The devil is in the details,<br/>but the
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    is in the experience.</h1>
+                    <img className="soul" src={require('../../assets/images/Soul.png').default} alt='soul'/>
+            </div>
+        <Header/>
+        </div>
     )
 }
 
